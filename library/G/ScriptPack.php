ad6a17846c6729b643cbb993343b9cdc53e54767
Florian Schwehn <fschwehn@gmail.com>
2011-09-25 23:24:46 +0200

    implemented distinct script pack delivery modes.

--- library/G/ScriptPack.php
+++ library/G/ScriptPack.php
@@ -5,7 +5,7 @@ class G_ScriptPack
 	/**
 	 * the delivery modes:
 	 */
-	const DeliverOnlyPackedScripts		= 0;
+	const DeliverOnlyPackedScript		= 0;
 	const DeliverOnlyCompiledScripts	= 1;
 	const DeliverMostRecent				= 2;
 	const DeliverOnlyCoffeeScripts		= 3;
@@ -50,6 +50,15 @@ class G_ScriptPack
 	}
 	
 	/**
+	 * set the delivery mode to one of the delivery mode class constans.
+	 * 
+	 * @param int $mode 
+	 */
+	public function setDeliveryMode($mode) {
+		$this->deliveryMode = (int)$mode;
+	}
+	
+	/**
 	 * determines which scripts will be deliverd as either coffe- plain java- or 
 	 * packed javascript. this depends on each script's processing state as well 
 	 * as on the delivery mode.
@@ -60,8 +69,64 @@ class G_ScriptPack
 		$javascripts = array();
 		$coffeescripts = array();
 		$publicPath = realpath(APPLICATION_PATH . '/../public');
-		
+
 		switch($this->deliveryMode) {
+			case self::DeliverOnlyPackedScript :
+				$scriptFile = '/js/packed/' . $this->name . '.js';
+				
+				if(!file_exists($publicPath . $scriptFile)) {
+					throw new Zend_Exception("Script '$scriptFile' could not be delivered!");
+				}
+				
+				$javascripts[] = $scriptFile;
+				break;
+			
+			case self::DeliverOnlyCompiledScripts : 
+				foreach($this->scriptNames as $scriptName) {
+					$compiledFile	= '/js/compiled/' . $scriptName . '.js';
+					$javascriptFile	= '/js/javascript/' . $scriptName . '.js';
+
+					$compiledFilePath	= $publicPath . $compiledFile;
+					$javascriptFilePath	= $publicPath . $javascriptFile;
+
+					$compiledFileExists		= file_exists($compiledFilePath);
+					$javascriptFileExists	= file_exists($javascriptFilePath);
+
+					if($compiledFileExists) {
+						$javascripts[] = $compiledFile;
+					}
+					else if($javascriptFileExists) {
+						$javascripts[] = $javascriptFile;
+					}
+					else {
+						throw new Zend_Exception("Script '$scriptName' could not be delivered!");
+					}
+				}
+				break;
+				
+			case self::DeliverOnlyCoffeeScripts : 
+				foreach($this->scriptNames as $scriptName) {
+					$coffeeFile		= '/js/coffee/' . $scriptName . '.coffee';
+					$javascriptFile	= '/js/javascript/' . $scriptName . '.js';
+
+					$coffeeFilePath		= $publicPath . $coffeeFile;
+					$javascriptFilePath	= $publicPath . $javascriptFile;
+
+					$coffeeFileExists		= file_exists($coffeeFilePath);
+					$javascriptFileExists	= file_exists($javascriptFilePath);
+
+					if($coffeeFileExists) {
+						$coffeescripts[] = $coffeeFile;
+					}
+					else if($javascriptFileExists) {
+						$javascripts[] = $javascriptFile;
+					}
+					else {
+						throw new Zend_Exception("Script '$scriptName' could not be delivered!");
+					}
+				}
+				break;
+				
 			case self::DeliverMostRecent : 
 				foreach($this->scriptNames as $scriptName) {
 					$coffeeFile		= '/js/coffee/' . $scriptName . '.coffee';
@@ -97,6 +162,7 @@ class G_ScriptPack
 					}
 				}
 				break;
+				
 			default:
 				throw new Zend_Exception("Unknown or unimplemented delivery mode ($this->deliveryMode)!");
 		}
